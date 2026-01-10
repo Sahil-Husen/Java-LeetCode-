b6f7fc61a36eae7539ae90e6916728ab8ad6d098
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-01-11 00:56:31 +0530

    String Problem Paranthesis Matching

---
+++ String/ParanthesisMatching.java
@@ -0,0 +1,31 @@
+package String;
+
+import java.util.Stack;
+
+public class ParanthesisMatching {
+    public static void main(String[] args) {
+        String str  = "{(])";
+        System.out.println(isMatched(str));
+    }
+
+    static boolean isMatched(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+
+            if (ch == '(' || ch == '{' || ch == '{') {
+                stack.push(ch);
+            } else {
+                if (stack.empty())  
+                    return false;
+                char top = stack.pop();
+                if ((ch == ')' && top != '(') ||
+                        (ch == '}' && top != '{') ||
+                        (ch == ']' && top != '['))
+                    return false;
+            }
+
+        }
+        return stack.empty();
+    }
+}
