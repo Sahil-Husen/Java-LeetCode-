2888c52d24ff66d419ea1128694b4e819542db18
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-01-11 11:39:47 +0530

    Paranthesis Matching and TwoSum unordered Problem solved on Leetcode

---
+++ Array/TwoSumUnordered.java
@@ -0,0 +1,13 @@
+package Array;
+
+import java.util.ArrayList;
+
+public class TwoSumUnordered {
+    public static void main(String[] args){
+
+    }
+
+    static int[] twoSum(int arr[], int target){
+        
+    }
+}
--- String/ParanthesisMatching.java
+++ String/ParanthesisMatching.java
@@ -4,7 +4,7 @@ import java.util.Stack;
 
 public class ParanthesisMatching {
     public static void main(String[] args) {
-        String str  = "{(])";
+        String str = "{(])";
         System.out.println(isMatched(str));
     }
 
@@ -16,7 +16,7 @@ public class ParanthesisMatching {
             if (ch == '(' || ch == '{' || ch == '{') {
                 stack.push(ch);
             } else {
-                if (stack.empty())  
+                if (stack.empty())
                     return false;
                 char top = stack.pop();
                 if ((ch == ')' && top != '(') ||
