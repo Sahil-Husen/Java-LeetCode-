60e18a7ae1d76800a3aadbdec4b61022ab0ae96f
Sahil-husen <sahilhussainsahil76@gmail.com>
2026-08-02 01:50:53 +0530

    Climbing Stair case Problem

---
+++ Placement_DSA/Array/ClimbingStaircase.java
@@ -0,0 +1,22 @@
+package Array;
+
+public class ClimbingStaircase {
+    public static int climb(int n){
+        int dp[] = new int[n+1];
+        dp[0]  =1;
+        dp[1]  =1;
+
+
+        for(int i=2;i<=n;i++){
+            dp[i] = dp[i-1]+dp[i-2];
+        }
+
+        return dp[n];
+
+    }
+
+    public static void main(String[] args) {
+        int stair = 5;
+        System.out.println(climb(stair));
+    }
+}
